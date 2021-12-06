b0b617f3a6c16c74377a921668ae005791d63457
lucanchling <luc.anchling@cpe.fr>
2021-12-06 10:53:30 +0100

    First Build

---
+++ build/CMakeFiles/projet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet.dir/src/main.cpp.o"
+  "projet"
+  "projet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tools/CMakeFiles/tools.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/luc/Documents/Jeu_TSI/tools/format/mesh_io_obj.cpp" "/home/luc/Documents/Jeu_TSI/build/tools/CMakeFiles/tools.dir/format/mesh_io_obj.cpp.o"
+  "/home/luc/Documents/Jeu_TSI/tools/format/mesh_io_off.cpp" "/home/luc/Documents/Jeu_TSI/build/tools/CMakeFiles/tools.dir/format/mesh_io_off.cpp.o"
+  "/home/luc/Documents/Jeu_TSI/tools/glhelper.cpp" "/home/luc/Documents/Jeu_TSI/build/tools/CMakeFiles/tools.dir/glhelper.cpp.o"
+  "/home/luc/Documents/Jeu_TSI/tools/mat4.cpp" "/home/luc/Documents/Jeu_TSI/build/tools/CMakeFiles/tools.dir/mat4.cpp.o"
+  "/home/luc/Documents/Jeu_TSI/tools/mesh.cpp" "/home/luc/Documents/Jeu_TSI/build/tools/CMakeFiles/tools.dir/mesh.cpp.o"
+  "/home/luc/Documents/Jeu_TSI/tools/tga.cpp" "/home/luc/Documents/Jeu_TSI/build/tools/CMakeFiles/tools.dir/tga.cpp.o"
+  "/home/luc/Documents/Jeu_TSI/tools/triangle_index.cpp" "/home/luc/Documents/Jeu_TSI/build/tools/CMakeFiles/tools.dir/triangle_index.cpp.o"
+  "/home/luc/Documents/Jeu_TSI/tools/vec2.cpp" "/home/luc/Documents/Jeu_TSI/build/tools/CMakeFiles/tools.dir/vec2.cpp.o"
+  "/home/luc/Documents/Jeu_TSI/tools/vec3.cpp" "/home/luc/Documents/Jeu_TSI/build/tools/CMakeFiles/tools.dir/vec3.cpp.o"
+  "/home/luc/Documents/Jeu_TSI/tools/vertex_opengl.cpp" "/home/luc/Documents/Jeu_TSI/build/tools/CMakeFiles/tools.dir/vertex_opengl.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
